6ccaa92fd45c7d450f548fca2983e906ab6a6711
江林羽 <371582812@qq.com>
2016-01-26 16:17:47 +0800

    Makefile

---
+++ src/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+#     Description:  TODO
+#
+#         Version:  1.0
+#         Created:  2015-12-08
+#        Modified:  2015-12-08
+#        Revision:  none
+#
+#          Author:  Zheng Yuhong , 371582812@qq.com
+#        HOMEPAGE:  zhengyh.com
+make ar
+rm -r build
